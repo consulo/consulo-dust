50c72b588e491e370010c038773c0ceac82c827a
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 14:15:05 +0300

    drop ide deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,24 @@
  * @since 15/01/2023
  */
 module com.linkedin.intellij.dust {
-  requires consulo.ide.api;
+  requires consulo.application.api;
+  requires consulo.code.editor.api;
+  requires consulo.color.scheme.api;
+  requires consulo.datacontext.api;
+  requires consulo.document.api;
+  requires consulo.language.api;
+  requires consulo.language.code.style.api;
+  requires consulo.language.editor.api;
+  requires consulo.language.impl;
+  requires consulo.localize.api;
+  requires consulo.project.api;
+  requires consulo.ui.api;
+  requires consulo.util.lang;
+  requires consulo.virtual.file.system.api;
+
   requires com.intellij.xml;
   requires com.intellij.xml.html.api;
-  
+
   exports com.linkedin.intellij.dust;
   exports com.linkedin.intellij.dust.parser;
   exports com.linkedin.intellij.dust.psi;
